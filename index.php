06264bcb7c7aa948b2be0c75a879335d6826a90c
Ivan <anu2biz@yandex.ru>
2017-01-25 19:43:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<!-- saved from url=(0023)https://bootswatch.com/ -->
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>IvanLis JQ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta property="og:type" content="Anu2biZ JQ">
+    <meta property="og:url" content="http://anu2biz.github.io/">
+    <meta property="og:image" content="http://anu2biz.guthub.io/img/logo.jpg">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="256">
+    <meta property="og:image:height" content="256">
+    <meta name="description" content="anu2biz JQ Dev">
+    <meta property="og:description" content="JQ Dev">
+    <meta property="og:title" content="Anu2biZ JQ">
+    <link rel="stylesheet" href="./css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="./css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/custom.min.css">
+
+    <link rel="stylesheet" href="./css/light.css">
+        <link rel="stylesheet" href="./css/slider.css">
+ </head>
+
+<body id="home">
+    <div class="navbar navbar-default navbar-fixed-top navbar-transparent" style="background: #141D27;">
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+            </div>
+            <div class="navbar-collapse collapse" id="navbar-main">
+                <ul class="nav navbar-nav">
+                    <li> <a href="">IvanLis:JQ</a> </li>
+                    <li> <a href="https://ivanlis.ru">Blog</a> </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="https://anu2biz.github.io" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                    <li><a href="https://vk.com/anu2biz" target="_blank"><i class="fa fa-vk" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="splash" style="background-position: 0px 0px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- <div><img class="logo" src="assets/img/logo.png"></div> -->
+                    <h1>{<i class="fa fa-chevron-left" aria-hidden="true"></i>omfortSlider}</h1> </div>
+            </div>
+        </div>
+    </div>
+    <div class="section-tout">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <h3><i class="fa fa-file-o"></i> Easy to Install</h3>
+                    <p>Download css/js files and start your own gallery</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <h3><i class="fa fa-github"></i> Open Source</h3>
+                    <p>This plug-in is licensed by <a href="https://github.com/Anu2biZ/comfortSlider/blob/master/LICENSE">MIT</a></p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <h3> <i class="fa fa-user"></i> Friendly</h3>
+                    <p>It's a responsive plugin for all the devices</p>
+                </div>
+            </div>
+        </div>
+    </div>
+ <div class="container">
+        <h3 class="text-center">Horizont Row Example</h3>
+        <div class="g-center" id="gallery">
+            <div> <img src="img/example/1.jpg" alt=""></div>
+            <div> <img src="img/example/1_1.jpg" alt=""></div>
+            <div> <img src="img/example/1.jpg" alt=""></div>
+            <div> <img src="img/example/1_1.jpg" alt=""></div>
+            <div> <img src="img/example/1.jpg" alt=""></div>
+            <div> <img src="img/example/1_1.jpg" alt=""></div>
+            <div> <img src="img/example/1.jpg" alt=""></div>
+            <div> <img src="img/example/1_1.jpg" alt=""></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="vertical" style="padding-top: 50px;">
+            <h3 class="text-center">Vertical Row Example</h3>
+            <div class="g-center" id="HZ">
+                <div> <img src="img/example/2.jpg" alt=""></div>
+                <div> <img src="img/example/2_1.jpg" alt=""></div>
+                <div> <img src="img/example/2.jpg" alt=""></div>
+                <div> <img src="img/example/2_1.jpg" alt=""></div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="vertical" style="padding-top: 50px;">
+            <h3 class="text-center">Mixed Row Example</h3>
+            <div class="g-center" id="MIXED">
+                <div> <img src="img/example/1.jpg" alt=""></div>
+                <div> <img src="img/example/2_1.jpg" alt=""></div>
+                <div> <img src="img/example/1_1.jpg" alt=""></div>
+                <div> <img src="img/example/2.jpg" alt=""></div>
+            </div>
+        </div>
+    </div> 
+    <div class="container">
+        <div class="vertical" style="padding-top: 50px;">
+            <h3 class="text-center">How it works</h3>
+            <br>
+            <h4 class="text-center"><b>comfortSlider</b> is a responsive plugin for your gallery with some comfortable options</h4>
+            <br>
+            <h4 class="text-center">Getting Started</h4>
+            <h5 class="text-center ">Include comfortSlider.css and comfortSlider.js to your project to start</h5>
+            <br>
+            <h4 class="text-center">Default</h4>
+            
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <pre class="slh__html">
+<div class="g-center" id="your_id">
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+</div>
+<!----- This is default template of html gallery ---->
+                    </pre>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <pre class="slh__js">
+                       
+
+ $('#your_id').comfortSlider();
+                   
+                    </pre>
+                </div>
+            </div>
+            
+            <br>
+            
+            <h3 class="text-center">Configuration</h3>
+            <br>
+            <h4 class="text-center">You can configure your gallery for devices to responsive last one, using such options as <kbd>lg</kbd> , <kbd>md</kbd>, <kbd>sm</kbd>, <kbd>xs</kbd> 
+             </h4>
+             
+       
+            
+            <br>
+            
+            <h4 class="text-center text-danger">Max size of LG - 6 | Max size of MD - 6 | Max size of SM - 4 | Max size of XS - 2</h4>
+            <br>
+            
+            <h4 class="text-center">Configure for devices</h4>
+            
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <pre class="slh__html">
+<div class="g-center" id="your_id">
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+    <div> <img src="img" alt=""></div>
+</div>
+<!----- This is default template of html gallery ---->
+                    </pre>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <pre class="slh__js">
+                       
+
+ $('#your_id').comfortSlider({
+                  'lg' : 3 , // 3 photos on big screens
+                  'md' : '3 , // 3 photos on notebooks
+                  'sm' : 2 , // 2 photos on tablets
+                  'xs' : 1 // 1 photo on smartphones
+                  });
+                   
+                    </pre>
+                </div>
+            </div>
+           
+           <br>
+           
+         <h4 class="text-center">Download it on <a href="">Github</a></h4>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/liteHighlighting.js"></script>
+    <script src="js/comfortSlider.js"></script>
+    <script>
+        $('#gallery').comfortSlider();
+        $('#HZ').comfortSlider();
+        $('#MIXED').comfortSlider();
+        
+    </script>
+
+</body>
